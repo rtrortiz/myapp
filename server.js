2bd3b5b69270bf94a00e932cd69b5b3f5baa8f48
Arturo Ortiz <ortiz4419@gmail.com>
2020-04-27 14:16:40 -0700

    added request and body validation. Also added User model

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 const express = require('express');
 const connectDB = require('./config/db');
-//const router = express.Router();
+
 
 const app = express();
 
 // Connect Database //
 connectDB();
 
+// Init Middleware //
+app.use(express.json({ extended: false}));
+
 app.get( '/', (req, res) => res.send('API Running'));
 
 // Define Routes
-app.use('/api/users', require('./api/users'));
-app.use('/api/posts', require('./api/posts'));
-app.use('/api/auth', require('./api/auth'));
-app.use('/api/profile', require('./api/profile'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
 
 const PORT = process.env.PORT || 5000;
 

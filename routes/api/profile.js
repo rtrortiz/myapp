c2bbc5f7b11e43bdf1334d91d05270b3a45c5330
Arturo Ortiz <ortiz4419@gmail.com>
2020-04-30 17:22:15 -0700

    modified api/profile.js

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,59 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator/check');
+const Profile = require('../../models/Profile');
+const User = require('../../models/Users');
+// @route GET api/profile/me
+// @desc GET current users profile
+// @access Private
+router.get('/me', auth, async (req, res) => {
+    try{
+        const profile = await Profile.findOne({ user: req.user.id }).populate('user', ['name', 'avatar']);
+        
+        if(!profile){
+            return res.status(400).json({msg: 'There is no profile for this user'})
+        }
+        
+        res.json(profile);
+        
+    } catch(err){
+        console.log(err.message);
+        res.status(500).send('Server Error');
+    }
+    
+});
 
-// you don't use app.get instead use router.get //
-// @route GET api/profile
-// @desc Test route
-// @access Public - this route does not need a token/auth for
-router.get('/', (req, res) => res.send('Profile route'));
+// @route POST api/profile/me
+// @desc Create or update user profile
+// @access Private
+router.post('/', [ auth, [
+    check('status', 'Status is required').not().isEmpty(),
+    check('skills', 'Skills is required').not().isEmpty()
+] ], 
+    async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+       return res.status(400).json({ errors: errors.array()});
+       }
+    
+     const {
+      company,
+      location,
+      website,
+      bio,
+      skills,
+      status,
+      githubusername,
+      youtube,
+      twitter,
+      instagram,
+      linkedin,
+      facebook
+    } = req.body;
+    
+    // Build profile object
+    const profileFields = {};
+});
 
 module.exports = router;

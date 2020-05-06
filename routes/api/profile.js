b72eaa087305cf6755ff686dbeba28ed404ec9cb
Arturo Ortiz <ortiz4419@gmail.com>
2020-05-06 09:37:17 -0700

    Add Profile Experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -159,7 +159,59 @@ router.delete('/', auth, async (req, res) => {
     
 });
 
+// @route PUT api/profile/experience
+// @desc Add profile experience
+// @access Private
+router.put('/', [auth, [
+    
+    check('title', 'Title is required').not().isEmpty(),
+    check('company', 'Company is required').not().isEmpty(),
+    check('from', 'From date is required').not().isEmpty(),
+] 
 
+    ], 
+async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array()});
+    }
+    
+    const {
+        title,
+        company,
+        location,
+        from,
+        to,
+        current,
+        description
+    } = req.body;
+    
+    // create an object with data that the user submits
+     const newExp = {
+        title,
+        company,
+        location,
+        from,
+        to,
+        current,
+        description
+    }
+    
+    try {
+        const profile = await Profile.findOne({ user: req.user.id });
+        profile.experience.unshift(newExp);
+        
+        await profile.save();
+        res.json(profile);
+    } catch(err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+}
+    
+    
+    
+    
+});
 
 
 

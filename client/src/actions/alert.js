f8cd862d2d91e5165077d082e0613c728bd7e135
Arturo Ortiz <ortiz4419@gmail.com>
2020-05-18 10:24:32 -0700

    fixed syntax errors and begun creating Alert Component and Action Call

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -1,7 +1,9 @@
+import { v4 as uuidv4 } from 'uuid';
 import { SET_ALERT, REMOVE_ALERT } from './types';
 
+
 export const setAlert = (msg, alertType) => dispatch => {
-    const id = uuid.v8();
+    const id = uuidv4();
     dispatch({
         type: SET_ALERT,
         payload: { msg, alertType, id}
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,10 @@
 import React, { Fragment, useState } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { setAlert } from '../../actions/alert';
+import PropTypes from 'prop-types';
 
-const Register = () => {
+const Register = ({ setAlert }) => {
     
     const [formData, setFormData] = useState({
         name:'',
@@ -15,12 +18,13 @@ const Register = () => {
     
     const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
     
-    const onSubmit = e => {
+    const onSubmit = async (e) => {
         e.preventDefault();
         if(password !== password2){
-            console.log('Passwords do not match');
+            setAlert('Passwords do not match', 'danger');
         } else {
-            console.log(formData);
+            //console.log(formData);
+            console.log('SUCCESS');
         }
     }
     
@@ -71,4 +75,10 @@ const Register = () => {
     
 }
 
-export default Register;
+
+Register.propTypes = {
+        setAlert: PropTypes.func.isRequired
+};
+        
+        
+export default connect(null, { setAlert })(Register);

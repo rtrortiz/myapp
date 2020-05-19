b661fd910340ef379162a0ad7c28d6a9b8e41e21
Arturo Ortiz <ortiz4419@gmail.com>
2020-05-18 18:54:03 -0700

    Alert Component and Action Call

--- client/src/components/layout/Alert.js
+++ client/src/components/layout/Alert.js
@@ -2,24 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-const Alert = ({ alerts }) => {
-    return ( <div>
-            
-            
-            
-            </div>)
-}
+
+const Alert = ({ alerts }) => alerts !== null && alerts.length > 0 && alerts.map(alert => (
+
+    <div key={alert.id} className={`alert alert-${alert.alertType}`}>
+        { alert.msg }
+    </div>
+));
+
+  
             
 Alert.propTypes = {
-            
-            alerts: PropTypes.array.isRequired
-            
-}
+    alerts: PropTypes.array.isRequired         
+};
             
 
-const.mapStateToProps = state => ({
+const mapStateToProps = state => ({
           alerts: state.alert  
 });
             
             
-export default connect()(Alert);
+export default connect(mapStateToProps)(Alert);

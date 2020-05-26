4b610ffb72ac4f262ce5c91f2cc2ab39d2eca858
Arturo Ortiz <ortiz4419@gmail.com>
2020-05-25 21:29:38 -0700

    Finished Edit Profile

--- client/src/components/profile-form/EditProfile.js
+++ client/src/components/profile-form/EditProfile.js
@@ -1,10 +1,10 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { createProfile } from '../../actions/profile';
+import { createProfile, getCurrentProfile } from '../../actions/profile';
 
-const CreateProfile = ({ createProfile, history }) => {
+const EditProfile = ({ profile: { profile, loading }, createProfile, getCurrentProfile, history }) => {
     const [formData, setFormData] = useState({
         company: '',
         website: '',
@@ -21,6 +21,51 @@ const CreateProfile = ({ createProfile, history }) => {
     });
     
     const [displaySocialInputs, toggleSocialInputs] = useState(false);
+    
+    /*useEffect(()=>{
+        
+    if (!profile) getCurrentProfile();
+    if (!loading && profile) {
+      const profileData = { ...initialState };
+      for (const key in profile) {
+        if (key in profileData) profileData[key] = profile[key];
+      }
+      for (const key in profile.social) {
+        if (key in profileData) profileData[key] = profile.social[key];
+      }
+      if (Array.isArray(profileData.skills))
+        profileData.skills = profileData.skills.join(', ');
+      setFormData(profileData);
+    }
+  }, [loading, getCurrentProfile, profile]);*/
+        
+    ////}//)//;
+    useEffect(() => {
+        getCurrentProfile();
+        
+        setFormData({
+            company: loading || !profile.company ? '' : profile.company,
+            website: loading || !profile.website ? '' : profile.website,
+            location: loading || !profile.location ? '' : profile.location,
+            status: loading || !profile.status ? '' : profile.status,
+            skills: loading || !profile.skills ? '' : profile.skills,
+            company: loading || !profile.company ? '' : profile.company,
+            githubusername: loading || !profile.githubusername ? '' : profile.githubusername,
+            bio: loading || !profile.bio ? '' : profile.bio,
+            twitter: loading || !profile.social ? '' : profile.social.twitter,
+            facebook: loading || !profile.social ? '' : profile.social.facebook,
+            linkedin: loading || !profile.social ? '' : profile.social.linkedin,
+            youtube: loading || !profile.social ? '' : profile.social.youtube,
+            instagram: loading || !profile.social ? '' : profile.social.instagram
+            
+        });   
+    });
+    
+    
+    
+    
+    
+    
     
     // change above to individual variables
     const {
@@ -42,8 +87,8 @@ const CreateProfile = ({ createProfile, history }) => {
     
     const onSubmit = e => {
         e.preventDefault();
-        createProfile(formData, history);
-    }
+        createProfile(formData, history, true);
+    };
     
     return (
     <Fragment>
@@ -160,9 +205,15 @@ const CreateProfile = ({ createProfile, history }) => {
     );
 };
 
-CreateProfile.propTypes = {
-    createProfile: PropTypes.func.isRequired
+EditProfile.propTypes = {
+    createProfile: PropTypes.func.isRequired,
+    getCurrentProfile: PropTypes.func.isRequired,
+    profile: PropTypes.object.isRequired
 };
 
+const mapStateToProps = state => ({
+    profile: state.profile
+});
 
-export default connect(null, { createProfile})(withRouter(CreateProfile));
+
+export default connect(mapStateToProps, { createProfile, getCurrentProfile })(withRouter(EditProfile));

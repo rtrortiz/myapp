8bb0889cf2c173b3597c55870843b1da9aa1dcf2
Arturo Ortiz <ortiz4419@gmail.com>
2020-05-27 18:04:18 -0700

    Started work on List Education and Experiences

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 import { getCurrentProfile } from '../../actions/profile';
 import Spinner from '../layout/Spinner';
 import DashboardActions from './DashboardActions';
+import Experience from './Experience';
 
 const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, loading } }) => {
     useEffect(() => {
@@ -14,14 +15,18 @@ const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, load
     return loading && profile === null ? <Spinner /> : <Fragment>
         <h1 className="large text-primary">Dashboard</h1>
         <p className='lead'><i className="fas fa-user"></i>Welcome { user && user.name } </p>
-        { profile !== null ? <Fragment><DashboardActions /></Fragment> : 
-            <Fragment>
+        { profile !== null ? (
+        <Fragment>
+            <DashboardActions />
+        </Fragment> 
+    
+            ):( 
+    
+        <Fragment>
                 <p>You have not yet setup a profile, please add some information</p>
                 <Link to='/create-profile' className='btn btn-primary my-1'>Create Profile</Link>
-        
-        
-            </Fragment> }
-        
+            </Fragment> 
+    )}
         </Fragment>;
      
 };

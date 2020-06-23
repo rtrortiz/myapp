0a790d3fcd35dcca81d6299679098cd49468f3b1
Arturo Ortiz <ortiz4419@gmail.com>
2020-06-23 13:01:49 -0700

    Started working on displaying profiles

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -7,9 +7,16 @@ import { logout } from '../../actions/auth';
 const Navbar = ({ auth: { isAuthenticated, loading}, logout }) => {
     const authLinks = (
      <ul>
-        <li><Link to="/dashboard">
+        <li>
+        <Link to="/profiles">
+        Educators
+        </Link>
+        </li>
+        <li>
+        <Link to="/dashboard">
         <i className="fas fa-user"></i>{' '} 
-        <span className="hide-sm">Dashboard</span></Link></li>
+        <span className="hide-sm">Dashboard</span></Link>
+        </li>
         <li>
             <a onClick={logout} href="#!">
             <i className="fas fa-sign-out-alt"></i>{' '} 
@@ -23,7 +30,7 @@ const Navbar = ({ auth: { isAuthenticated, loading}, logout }) => {
     
     const guestLinks = (
      <ul>
-        <li><a href="#!">Educators</a></li>
+        <li><Link to="/profiles">Educators</Link></li>
         <li><Link to="/register">Register</Link></li>
         <li><Link to="/login">Login</Link></li>
       </ul>
--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,4 +1,4 @@
-import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE, UPDATE_PROFILE } from '../actions/types';
+import { GET_PROFILE, GET_PROFILES, PROFILE_ERROR, CLEAR_PROFILE, UPDATE_PROFILE } from '../actions/types';
 
 
 // State
